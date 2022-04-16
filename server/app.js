f2191bfd189040c094dbec94f14ad0d2f1ac833d
mustafa <mustafamjaber@yahoo.com>
2022-04-16 16:34:39 +0200

    successfully connected to the database

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const authRouter = require("./routers/auth");
+const connectDb = require("./db/connect");
 
 app.use(cors());
 app.use(express.json());
@@ -11,4 +12,12 @@ app.use("/api/v1/auth", authRouter);
 
 const port = process.env.PORT || 4200;
 
-app.listen(port, () => console.log(`app is listening on port ${port}`));
+const start = async () => {
+  try {
+    await connectDb(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`app is listening on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
