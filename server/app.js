42bbc9f7c7339b22b2c9ba0ff1fad26cc17a778b
mustafa <mustafamjaber@yahoo.com>
2022-04-17 14:11:06 +0200

    added the jwt and successfully register a user

--- server/app.js
+++ server/app.js
@@ -8,12 +8,15 @@ const connectDb = require("./db/connect");
 app.use(cors());
 app.use(express.json());
 
+//*the auth router
 app.use("/api/v1/auth", authRouter);
 
 const port = process.env.PORT || 4200;
 
+//*start the server
 const start = async () => {
   try {
+    //*connect to the database
     await connectDb(process.env.MONGO_URI);
     app.listen(port, () => console.log(`app is listening on port ${port}`));
   } catch (error) {
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,8 +1,13 @@
-const register = (req, res) => {
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const register = async (req, res) => {
   try {
-    res.status(200).send("you arr registered");
+    const user = await User.create({ ...req.body });
+    const token = user.createJWT();
+    res.status(200).json({ user: { name: user.name }, token });
   } catch (error) {
-    res.status(400).json(error);
+    res.status(400).json({ message: error.message });
+    console.log(error);
   }
 };
 const login = (req, res) => {
